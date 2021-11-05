8b3ca3137f7b16cf6183b476479cda90999a9f4d
vandenman <donvdbergh@hotmail.com>
2021-11-05 10:31:25 +0100

    expanded lkj tests

--- simulations/demos/lkj_tests.jl
+++ simulations/demos/lkj_tests.jl
@@ -1,4 +1,4 @@
-using Turing, MCMCChains, Plots, Distributions, LinearAlgebra, KernelDensity
+using Turing, MCMCChains, FillArrays, Plots, Distributions, LinearAlgebra, KernelDensity
 import DynamicPPL: @submodel
 
 """
@@ -86,11 +86,52 @@ for i in eachindex(plts)
 
 	K, η, ρs = results[i]
 	density_estimate = kde(view(ρs, :, 1); npoints = 2^12, boundary = (-1, 1))
-	plt = plot(density_estimate.x, density_estimate.density, title = "K = $K, η = $η")
-	plot!(plt, xcoords, dlkjcorr_marginal.(xcoords, K, η))
+	plt = plot(density_estimate.x, density_estimate.density, title = "K = $K, η = $η", legend = isone(i) ? :outerright : false,
+	           label = "sampled")
+	plot!(plt, xcoords, dlkjcorr_marginal.(xcoords, K, η), label = "theoretical")
 	plts[i] = plt
 
 end
 
 plot(plts..., layout = (length(Ks), length(ηs)))
 
+# use the LKJ to fit a normal
+
+n, p = 100, 4
+
+true_means  = collect(1.0:p)
+true_R_chol = rand(LKJCholesky(p, 1.0, 'U'))
+true_R      = true_R_chol.U'true_R_chol.U
+true_sd     = collect(1.0:p)
+true_S_chol = true_R_chol.U * Diagonal(true_sd)
+true_S      = true_S_chol'true_S_chol
+
+x = rand(MvNormal(true_means, true_S), n)
+
+@model function mvnormal_turing(x, η, ::Type{T} = Float64) where T
+
+	p, n = size(x)
+	means ~ MvNormal(ones(Float64, p))
+	sds   ~ filldist(InverseGamma(1.0, 1.0), p)
+
+	R_chol = @submodel $(Symbol("manual_lkj")) manual_lkj2(p, η, T)
+	Sigma_chol = UpperTriangular(R_chol * Diagonal(sds))
+
+	if det(Sigma_chol) < sqrt(eps(T))
+		Turing.@addlogprob! -Inf
+		return (R_chol, )
+	end
+	Sigma = Sigma_chol'Sigma_chol
+	
+
+	for i in 1:n
+		x[:, i] ~ MvNormal(means, Sigma)
+	end
+
+	return (R_chol, )
+end
+
+mod_mvnormal = mvnormal_turing(x, 1.0)
+samps = sample(mod_mvnormal, NUTS(), 100)
+
+Sigma_chol = [1.6996185109323887 4.861521935132051 -0.6498621746830178 0.21264491910724162; 0.0 7.27536669578297 0.6005138605151561 0.5164578717483551; 0.0 0.0 0.8337881313363649 -0.3674260700803254; 0.0 0.0 0.0 0.35013387861256073]
