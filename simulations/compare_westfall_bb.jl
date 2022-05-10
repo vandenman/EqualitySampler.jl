26d4c85b07e8cb0bf6366c2b7df05f0f39f05820
Don van den Bergh <donvdbergh@hotmail.com>
2022-05-10 21:48:51 +0200

    explore inconsistencies between population and sample

--- simulations/compare_westfall_bb.jl
+++ simulations/compare_westfall_bb.jl
@@ -7,19 +7,22 @@ obs_per_group = 50
 hypothesis = :p75
 n_groups = 5
 offset = 0.2
-mcmc_settings = Simulations.MCMCSettings(;iterations = 10_000, burnin = 1, chains = 1)
+mcmc_settings = Simulations.MCMCSettings(;iterations = 10_000, burnin = 1000, chains = 1)
 mcmc_settings2 = MCMCSettings()
 partition_prior = BetaBinomialMvUrnDistribution(n_groups, 1, binomial(n_groups, 2))
 
 n_rep = 100
 df_results = DataFrame(
-	model          = Vector{Vector{Int}}(undef, n_rep),
-	perf_w         = Vector{Int}(undef, n_rep),
-	perf_bb        = Vector{Int}(undef, n_rep),
-	perf_bb2       = Vector{Int}(undef, n_rep),
-	post_probs_w   = Vector{Matrix{Float64}}(undef, n_rep),
-	post_probs_bb  = Vector{Matrix{Float64}}(undef, n_rep),
-	post_probs_bb2 = Vector{Matrix{Float64}}(undef, n_rep),
+	model           = Vector{Vector{Int}}(undef, n_rep),
+	perf_w          = Vector{Int}(undef, n_rep),
+	perf_bb         = Vector{Int}(undef, n_rep),
+	# perf_bb2        = Vector{Int}(undef, n_rep),
+	post_probs_w    = Vector{Matrix{Float64}}(undef, n_rep),
+	post_probs_bb   = Vector{Matrix{Float64}}(undef, n_rep),
+	# post_probs_bb2  = Vector{Matrix{Float64}}(undef, n_rep),
+	data_means      = Vector{Vector{Float64}}(undef, n_rep),
+	data_vars       = Vector{Vector{Float64}}(undef, n_rep),
+	data_ns         = Vector{Vector{Int}}(undef, n_rep),
 )
 
 seeds = 1:100
@@ -38,30 +41,111 @@ ProgressMeter.@showprogress for i in 1:n_rep
 	post_probs_w = fit_w.log_posterior_odds_mat
 
 	chain  = Simulations.anova_test(dat, partition_prior; mcmc_settings = mcmc_settings,  rng = rng, modeltype = :reduced, spl = 0.05)
-	chain2 = Simulations.anova_test(dat, partition_prior; mcmc_settings = mcmc_settings2, rng = rng, modeltype = :reduced, spl = 0.05)
+	# chain2 = Simulations.anova_test(dat, partition_prior; mcmc_settings = mcmc_settings2, rng = rng, modeltype = :reduced, spl = 0.05)
 
 	partition_samples = MCMCChains.group(chain, :partition).value.data
 	post_probs = Simulations.compute_post_prob_eq(partition_samples)
 
-	partition_samples = MCMCChains.group(chain2, :partition).value.data
-	post_probs2 = Simulations.compute_post_prob_eq(partition_samples)
+	# partition_samples = MCMCChains.group(chain2, :partition).value.data
+	# post_probs2 = Simulations.compute_post_prob_eq(partition_samples)
 
 	perf_w   = prop_incorrect_αβ(post_probs_w, true_model, true)
 	perf_bb  = prop_incorrect_αβ(post_probs,   true_model, false)
-	perf_bb2 = prop_incorrect_αβ(post_probs2,   true_model, false)
+	# perf_bb2 = prop_incorrect_αβ(post_probs2,   true_model, false)
 
 	df_results[i, :model]          = true_model
 	df_results[i, :perf_w]         = perf_w.α_error_prop
 	df_results[i, :perf_bb]        = perf_bb.α_error_prop
-	df_results[i, :perf_bb2]       = perf_bb2.α_error_prop
+	# df_results[i, :perf_bb2]       = perf_bb2.α_error_prop
 	df_results[i, :post_probs_w]   = post_probs_w
 	df_results[i, :post_probs_bb]  = post_probs
-	df_results[i, :post_probs_bb2] = post_probs2
+	# df_results[i, :post_probs_bb2] = post_probs2
+
+	obs_mean, obs_var, obs_n = Simulations.get_suff_stats(dat)
+	df_results[i, :data_means] = obs_mean
+	df_results[i, :data_vars] = obs_var
+	df_results[i, :data_ns] = obs_n
 
 end
 
-(mean(df_results[!, :perf_w]), mean(df_results[!, :perf_bb]), mean(df_results[!, :perf_bb2]))
-df_sub = subset(df_results, Cols(:perf_w, :perf_bb, :perf_bb2) => (w, bb, bb2) -> w .> 0 .|| bb .> 0 .|| bb2 .> 0)
+(mean(df_results[!, :perf_w]), mean(df_results[!, :perf_bb]))#, mean(df_results[!, :perf_bb2]))
+df_sub = subset(df_results, Cols(:perf_w, :perf_bb) => (w, bb) -> w .> 0 .| bb .> 0)
+# df_sub = subset(df_results, Cols(:perf_w, :perf_bb, :perf_bb2) => (w, bb, bb2) -> w .> 0 .|| bb .> 0 .|| bb2 .> 0)
+
+idx_bad = findall(>=(1), df_results.perf_bb)
+df_sub = df_results[idx_bad, :]
+
+function compute_obs_mean_abs_diff(obs_mean)
+	obs_mean_diff_mat = zeros(5, 5)
+	for i in 1:4, j in i+1:5
+		obs_mean_diff_mat[j, i] = abs(obs_mean[i] - obs_mean[j])
+		obs_mean_diff_mat[i, j] = obs_mean_diff_mat[j, i]
+	end
+	obs_mean_diff_mat
+end
+
+# function check_consistency_model_pairwise_differences(model, obs_mean)
+# 	obs_mean_diff_mat = compute_obs_mean_abs_diff(obs_mean)
+# 	for i in axes(obs_mean_diff_mat, 1)
+# 		obs_mean_diff_mat[i, i] = Inf
+# 	end
+# 	_, idx = findmin(obs_mean_diff_mat)
+# 	consistent = model[idx[1]] == model[idx[2]]
+# 	return consistent
+# end
+
+function check_consistency_model_pairwise_differences(model, obs_mean, tol = .3)
+	obs_mean_diff_mat = compute_obs_mean_abs_diff(obs_mean)
+	for i in axes(obs_mean_diff_mat, 1)
+		obs_mean_diff_mat[i, i] = Inf
+	end
+
+	cc = EqualitySampler.fast_countmap_partition_incl_zero(model)
+	i1, i2 = 0, 0
+	for i in eachindex(model)
+		if cc[model[i]] > 1
+			if i1 == 0
+				i1 = i
+			else
+				i2 = i
+			end
+		end
+	end
+
+	obs_mean_diff_mat_v = view(obs_mean_diff_mat, :, [i1, i2])
+
+	_, idx = findmin(obs_mean_diff_mat_v)
+	idx12 = CartesianIndex(i1, i2)
+	if idx[2] == 1
+		idxd1 = CartesianIndex(i1, idx[1])
+		idxd2 = CartesianIndex(i2, idx[1])
+	else
+		idxd1 = CartesianIndex(i1, idx[2])
+		idxd2 = CartesianIndex(i2, idx[2])
+	end
+	return obs_mean_diff_mat[idxd2] <= tol
+end
+
+model, obs_mean = df_sub.model[1], df_sub.data_means[1]
+model, obs_mean = df_results.model[1], df_results.data_means[1]
+model, obs_mean = df_results.model[51], df_results.data_means[51]
+check_consistency_model_pairwise_differences(df_sub.model[1], df_sub.data_means[1])
+check_consistency_model_pairwise_differences(df_results.model[1], df_results.data_means[1])
+
+check_consistency_model_pairwise_differences.(df_sub.model, df_sub.data_means)
+
+
+df_results2 = transform(df_results, 
+	[:model, :data_means] => ((m, d) -> check_consistency_model_pairwise_differences.(m, d, .2)) => :consistent
+)
+df_results2[!, :i] = axes(df_results2, 1)
+
+sort(df_results2, order(:perf_bb))
+
+df_sub.model[1]
+compute_obs_mean_abs_diff(df_sub.data_means[1])
+
+compute_obs_mean_abs_diff(df_sub.data_means[2])
 
 df_sub[1, :model]
 df_sub[1, :post_probs_w]
@@ -83,12 +167,14 @@ model_probs[join(EqualitySampler.reduce_model(true_model))]
 
 obs_mean, obs_var, obs_n = Simulations.get_suff_stats(dat)
 obs_mean
-obs_mean_diff_mat = zeros(5, 5)
-for i in 1:4, j in i+1:5
-	obs_mean_diff_mat[j, i] = abs(obs_mean[i] - obs_mean[j])
-	obs_mean_diff_mat[i, j] = obs_mean_diff_mat[j, i]
+function compute_obs_mean_abs_diff(obs_mean)
+	obs_mean_diff_mat = zeros(5, 5)
+	for i in 1:4, j in i+1:5
+		obs_mean_diff_mat[j, i] = abs(obs_mean[i] - obs_mean[j])
+		obs_mean_diff_mat[i, j] = obs_mean_diff_mat[j, i]
+	end
+	obs_mean_diff_mat
 end
-
 #=
 5×5 Matrix{Float64}:
 0.0       0.218196  0.862712  0.513432  0.383002
--- simulations/demos/variances.jl
+++ simulations/demos/variances.jl
@@ -1,4 +1,4 @@
-using EqualitySampler, Turing, Plots, FillArrays, Plots.PlotMeasures, Colors
+using EqualitySampler, EqualitySampler.Simulations, Turing, Plots, FillArrays, Plots.PlotMeasures, Colors
 using Chain
 using MCMCChains
 import	DataFrames		as DF,
@@ -8,12 +8,11 @@ import	DataFrames		as DF,
 		CSV
 import DynamicPPL: @submodel, VarInfo
 import Distributions
-
+import KernelDensity
 import Printf
 
 #region functions
-
-include("../anovaFunctions.jl")
+include("../simulation_helpers.jl")
 include("lkj_prior.jl")
 
 round_2_decimals(x::Number) = Printf.@sprintf "%.2f" x
@@ -34,7 +33,7 @@ function triangular_to_vec(x::LA.UpperTriangular{T, Matrix{T}}) where T
 	p = size(x, 1)
 	result = Vector{T}(undef, p * (p + 1) ÷ 2)
 	idx = 1
-	for i in 1:p
+	@inbounds for i in 1:p
 		for j in 1:i
 			result[idx] = x[j, i]
 			idx += 1
@@ -78,11 +77,23 @@ end
 function extract_partition_samples(model, chn)
 
 	gen = generated_quantities(model, Turing.MCMCChains.get_sections(chn, :parameters))
-	partition_samples = Matrix{Int}(undef, length(gen[1][end]), length(gen))
+	partition_samples = Matrix{Int}(undef, length(gen), length(gen[1][end]))
 	for i in eachindex(gen)
-		partition_samples[:, i] .= reduce_model(gen[i][end])
+		partition_samples[i, :] .= reduce_model(gen[i][end])
 	end
 	return partition_samples
+end
+
+function extract_σ_samples(model, chn)
+
+	gen = generated_quantities(model, Turing.MCMCChains.get_sections(chn, :parameters))
+	k = length(gen[1][1])
+	σ_samples = Matrix{Float64}(undef, length(gen), 2k)
+	@inbounds for i in eachindex(gen)
+		σ_samples[i, 1:k]    .= gen[i][1]
+		σ_samples[i, k+1:2k] .= gen[i][2]
+	end
+	return σ_samples
 end
 
 #endregion
@@ -100,7 +111,7 @@ end
 	ρ ~ Dirichlet(Ones(2p))
 	τ ~ JeffreysPriorVariance()
 
-	ρ_constrained = isnothing(partition) ? ρ : average_equality_constraints(ρ, partition)
+	ρ_constrained = isnothing(partition) ? ρ : EqualitySampler.Simulations.average_equality_constraints(ρ, partition)
 
 	σ_m = τ .* view(ρ_constrained,   1: p)
 	σ_w = τ .* view(ρ_constrained, p+1:2p)
@@ -138,15 +149,32 @@ end
 
 #endregion
 
+#region plot functions
+
+function compute_density_estimate(mat, npoints = 2^12)
+	no_groups = size(mat, 2)
+	x = Matrix{Float64}(undef, npoints, no_groups)
+	y = Matrix{Float64}(undef, npoints, no_groups)
+
+	for (i, col) in enumerate(eachcol(mat))
+		k = KernelDensity.kde(col; npoints = npoints)#, boundary = (0, Inf))
+		x[:, i] .= k.x
+		y[:, i] .= k.density
+	end
+
+	return (x = x, y = y)
+end
+#endregion
+
 #region simulated data
 # let's test the model on simulated data
 n, p = 1_000, 5
 
 μ_1 = randn(p)
 μ_2 = randn(p)
-τ   = 1.0
+τ   = 5.0 # with 1.0 everything is too small
 partition = rand(UniformMvUrnDistribution(2p))
-ρ_constrained = average_equality_constraints(rand(Dirichlet(ones(2p))), partition)
+ρ_constrained = EqualitySampler.Simulations.average_equality_constraints(rand(Dirichlet(ones(2p))), partition)
 σ_1, σ_2 = τ .* view(ρ_constrained,   1: p), τ .* view(ρ_constrained, p+1:2p)
 
 R_chol_1, R_chol_2 = rand(Distributions.LKJCholesky(p, 1.0), 2)
@@ -167,40 +195,67 @@ observed_values = (obs_mean_1, obs_mean_2, obs_sd_1, obs_sd_2, triangular_to_vec
 true_values     = (μ_1, μ_2, σ_1, σ_2, triangular_to_vec(Σ_chol_1), triangular_to_vec(Σ_chol_2))
 
 mod_var_ss_full = varianceMANOVA_suffstat(obs_mean_1, obs_cov_chol_1, n_1, obs_mean_2, obs_cov_chol_2, n_2)
-chn_full = sample(mod_var_ss_full, NUTS(), 5_000)
+chn_full = sample(mod_var_ss_full, NUTS(), 15_000)
 gen = generated_quantities(mod_var_ss_full, Turing.MCMCChains.get_sections(chn_full, :parameters))
 
 post_means_eq = extract_means_continuous_params(mod_var_ss_full, chn_full)
 plot_retrieval(observed_values, post_means_eq, string.(keys(post_means_eq)))
 plot_retrieval(true_values, post_means_eq, string.(keys(post_means_eq)))
-
-
-# temp_hcat(x, y) = hcat(x, y, x .- y)
-# temp_hcat(post_mean_μ_1, obs_mean_1)
-# temp_hcat(post_mean_μ_2, obs_mean_2)
-# temp_hcat(post_mean_σ_1, obs_sd_1)
-# temp_hcat(post_mean_σ_2, obs_sd_2)
-# temp_hcat(post_mean_Σ_chol_1, triangular_to_vec(obs_cov_chol_1))
-# temp_hcat(post_mean_Σ_chol_2, triangular_to_vec(obs_cov_chol_2))
-
-# temp_hcat(post_mean_μ_1, μ_1)
-# temp_hcat(post_mean_μ_2, μ_2)
-# temp_hcat(post_mean_σ_1, σ_1)
-# temp_hcat(post_mean_σ_2, σ_2)
-# temp_hcat(post_mean_Σ_chol_1, triangular_to_vec(Σ_chol_1))
-# temp_hcat(post_mean_Σ_chol_2, triangular_to_vec(Σ_chol_2))
+# TODO: sometimes the plots above look like the chains didn't converge
 
 partition_prior = BetaBinomialMvUrnDistribution(2p, 1, 1)
 mod_var_ss_eq = varianceMANOVA_suffstat_equality_selector(obs_mean_1, obs_cov_chol_1, n_1, obs_mean_2, obs_cov_chol_2, n_2, partition_prior)
-spl = get_sampler(mod_var_ss_eq, 0.0)
-chn_eq = sample(mod_var_ss_eq, spl, 50000)
+spl = EqualitySampler.Simulations.get_sampler(mod_var_ss_eq, :custom, 0.001)
+chn_eq = sample(mod_var_ss_eq, spl, 10000)
 
 post_means_eq = extract_means_continuous_params(mod_var_ss_eq, chn_eq)
 plot_retrieval(observed_values, post_means_eq, string.(keys(post_means_eq)))
+# these do not look good!
 
 partition_samples = extract_partition_samples(mod_var_ss_eq, chn_eq)
+post_probs_eq = compute_post_prob_eq(partition_samples)
+prop_incorrect_αβ(post_probs_eq, partition, false) # <- not good!
 
-compute_post_prob_eq()
+var_samples_full = extract_σ_samples(mod_var_ss_full, chn_full)
+var_samples_eq   = extract_σ_samples(mod_var_ss_eq,   chn_eq)
+
+plot(var_samples_full[:, 1])
+plot(var_samples_eq[2000:end, 1]) # <- does NOT look good!
+
+
+density_est_full = compute_density_estimate(var_samples_full)
+density_est_eq   = compute_density_estimate(var_samples_eq)
+
+plot(density_est_full.x, density_est_full.y, legend = true)
+plot(density_est_eq.x,   density_est_eq.y, legend = true)
+
+# heatmap
+# x_nms = journal_data[!, :journal]# names(eq_table)[1]
+# color_gradient = cgrad(cgrad(ColorSchemes.magma)[0.15:0.01:1.0])
+# annotations = []
+# for i in 1:7, j in i+1:8
+# 	z = eq_table[9-i, 9-j]
+# 	col = color_gradient[1 - z]
+# 	push!(annotations,
+# 		(
+# 			8 - j + 0.5,
+# 			i - 0.5,
+# 			Plots.text(
+# 				round_2_decimals(z),
+# 				8, col, :center
+# 			)
+# 		)
+# 	)
+# end
+
+# right_panel = heatmap(x_nms, reverse(x_nms), Matrix(eq_table)[8:-1:1, :],
+# 	aspect_ratio = 1, showaxis = false, grid = false, color = color_gradient,
+# 	clims = (0, 1),
+# 	title = "Posterior probability of pairwise equality",
+# 	#=colorbar_ticks = 0:.2:1, <- only works with pyplot =#
+# 	annotate = annotations,
+# 	xmirror = false);
+
 
 
 #endregion
