764f7e80bfc85f8319309cafc44ce33d7f44815b
vandenman <donvdbergh@hotmail.com>
2022-06-27 14:36:56 +0200

    westfall & pairwise priors now make some sense

--- simulations/big_simulation_priors_analysis.jl
+++ simulations/big_simulation_priors_analysis.jl
@@ -1,7 +1,69 @@
-using EqualitySampler, Plots, Statistics, NamedArrays, ProgressMeter
+using EqualitySampler, Plots, Statistics, NamedArrays, ProgressMeter, Plots.PlotMeasures
 using Printf
+import LogExpFunctions
 include("priors_plot_colors_shapes_labels.jl")
 include("simulation_helpers.jl")
+
+# PairwiseMvUrnDistribution is not valid in general and only logpdf_model_distinct and pdf_model_distinct make sense
+function _pairwiseMvUrnDistribution_helper(m, k, logp, logip)
+	no_parameters = count_parameters(m)
+	no_equal_pairs   = k - no_parameters + 1
+	no_unequal_pairs = no_parameters - 1
+	return no_equal_pairs * logp + no_unequal_pairs * logip
+end
+
+struct PairwiseMvUrnDistribution{T <: Integer} <: AbstractMvUrnDistribution{T}
+	k::T
+	p::Float64
+	_logZ::Float64
+	function PairwiseMvUrnDistribution(k::T, p::AbstractFloat = 0.5, logZ::AbstractFloat = 0.0) where T<:Integer
+		new{T}(k, p, logZ)
+	end
+end
+function PairwiseMvUrnDistribution(k::T, p::AbstractFloat = 0.5) where T<:Integer
+	# logZ = LogExpFunctions.sum(
+	# 	m->_pairwiseMvUrnDistribution_helper(m, k, log(p), log1p(-p)),
+	# 	EqualitySampler.DistinctModelsIterator(k)
+	# )
+	logZ = LogExpFunctions.logsumexp(_pairwiseMvUrnDistribution_helper(m, k, log(p), log1p(-p)) for m in EqualitySampler.DistinctModelsIterator(k))
+	PairwiseMvUrnDistribution(k, p, logZ)
+end
+
+EqualitySampler.logpdf_model_distinct(d::PairwiseMvUrnDistribution, x::AbstractVector{<:Integer}) = logpdf_model_distinct(d, count_parameters(x))
+function EqualitySampler.logpdf_model_distinct(d::PairwiseMvUrnDistribution, no_parameters::Integer)
+	EqualitySampler.in_eqsupport(d, no_parameters) || return -Inf
+	no_equal_pairs   = d.k - no_parameters + 1
+	no_unequal_pairs = no_parameters - 1
+	no_equal_pairs * log(d.p) + no_unequal_pairs * log1p(-d.p) - d._logZ
+end
+function westfallMvUrnDistribution(k::T, pH0::AbstractFloat = 0.5, logZ::AbstractFloat = NaN) where T<:Integer
+	τ = pH0 ^ (1 / k)
+	# logτ = Float64((1 / k) * log(pH0))
+	isnan(logZ) ? PairwiseMvUrnDistribution(k, Float64(τ)) : PairwiseMvUrnDistribution(k, Float64(τ), logZ)
+end
+
+# pdf_model_distinct(westfallMvUrnDistribution(5, .5), [1, 1, 1, 1, 1]) ≈ .5
+@assert pdf_model_distinct(westfallMvUrnDistribution(5, .5, 0.0), [1, 1, 1, 1, 1]) ≈ .5
+@assert pdf_model_distinct(PairwiseMvUrnDistribution(5, .5, 0.0), [1, 1, 1, 1, 1]) ≈ .5^5
+@assert pdf_model_distinct(PairwiseMvUrnDistribution(5, .6, 0.0), [1, 1, 1, 2, 3]) ≈ .6^3 * .4^2
+
+mmm = Matrix(EqualitySampler.DistinctModelsIterator(5))
+d = westfallMvUrnDistribution(5, .5, 0.0)
+ppp = pdf_model_distinct.(Ref(d), eachcol(mmm))
+d2 = PairwiseMvUrnDistribution(5, 0.8705505632961241)
+ppp2 = pdf_model_distinct.(Ref(d2), eachcol(mmm))
+d3 = PairwiseMvUrnDistribution(5, 0.5)
+ppp3 = pdf_model_distinct.(Ref(d3), eachcol(mmm))
+[sum(ppp); sum(ppp2); sum(ppp3)]
+
+dp = PairwiseMvUrnDistribution(5, .5)
+dw = westfallMvUrnDistribution(5, .5)
+[pdf_model_distinct(d, [1, 1, 1, 1, 1]) for d in (dp, dw)]
+[pdf_model_distinct(d, [1, 1, 1, 1, 2]) for d in (dp, dw)]
+[pdf_model_distinct(d, [1, 1, 1, 2, 2]) for d in (dp, dw)]
+[pdf_model_distinct(d, [1, 1, 2, 2, 2]) for d in (dp, dw)]
+[pdf_model_distinct(d, [1, 1, 2, 2, 3]) for d in (dp, dw)]
+
 
 function compute_model_errors(true_ρ, ρ)
 	α_error_count = 0
@@ -29,14 +91,18 @@ function compute_model_errors(true_ρ, ρ)
 end
 
 # function barrier
-function get_prob_ρ(s, ρ)
+function get_prob_ρ(s, ρ, z_PairwiseMvUrnDistribution, z_westfallMvUrnDistribution)
 	if s !== :Westfall && s !== :Westfall_uncorrected
 		d = instantiate_prior(s, k)
 		prob_ρ = pdf_model_distinct(d, ρ)
-	elseif s !== :Westfall_uncorrected
-		prob_ρ = pdf_model_distinct(UniformMvUrnDistribution(k), ρ)
+	elseif s === :Westfall_uncorrected
+		d = PairwiseMvUrnDistribution(k, .5, z_PairwiseMvUrnDistribution)
+		prob_ρ = pdf_model_distinct(d, ρ)
+		# prob_ρ = pdf_model_distinct(UniformMvUrnDistribution(k), ρ)
 	else #s !== :Westfall
-		prob_ρ = pdf_model_distinct(UniformMvUrnDistribution(k), ρ)
+		d = westfallMvUrnDistribution(k, .5, z_westfallMvUrnDistribution)
+		prob_ρ = pdf_model_distinct(d, ρ)
+		# prob_ρ = pdf_model_distinct(UniformMvUrnDistribution(k), ρ)
 	end
 	return prob_ρ::Float64
 end
@@ -46,6 +112,10 @@ function compute_prior_performance(k::Integer, priors_sym, hypotheses)
 	α_errors = zeros(length(priors_sym), length(hypotheses))
 	β_errors = zeros(length(priors_sym), length(hypotheses))
 
+	z_PairwiseMvUrnDistribution = PairwiseMvUrnDistribution(k, .5)._logZ
+	τ = 0.5 ^ (1 / k)
+	z_westfallMvUrnDistribution = LogExpFunctions.logsumexp(_pairwiseMvUrnDistribution_helper(m, k, log(τ),  log1p(-τ))  for m in EqualitySampler.DistinctModelsIterator(k))
+
 	@showprogress for true_ρ in EqualitySampler.DistinctModelsIterator(k)
 		j = EqualitySampler.count_parameters(true_ρ)
 		for ρ in EqualitySampler.DistinctModelsIterator(k)
@@ -54,7 +124,7 @@ function compute_prior_performance(k::Integer, priors_sym, hypotheses)
 
 			for (l, s) in enumerate(priors_sym)
 
-				prob_ρ = get_prob_ρ(s, ρ)
+				prob_ρ = get_prob_ρ(s, ρ, z_PairwiseMvUrnDistribution, z_westfallMvUrnDistribution)
 				α_fam_errors[l, j] += α_fam_error  * prob_ρ
 				α_errors[l, j]     += α_prop_error * prob_ρ
 				β_errors[l, j]     += β_prop_error * prob_ρ
@@ -68,15 +138,12 @@ end
 # @code_warntype compute_prior_performance(k, nsim, priors_sym, hypotheses)
 
 k = 5
-nsim = 1_000
 priors_sym = get_priors()
+priors_sym = priors_sym[[1:2; 4:7; 9:11]] # drop :BetaBinomialk1 and :DirichletProcess2_0
+priors_sym = priors_sym[[2:9; 1]] # reorder
 hypotheses = (:p00, :p25, :p50, :p75, :p100)
 
 α_fam_errors, α_errors, β_errors = compute_prior_performance(k, priors_sym, hypotheses)
-
-α_fam_errors_avg = dropdims(mean(α_fam_errors; dims = 3); dims = 3)
-α_errors_avg     = dropdims(mean(α_errors;     dims = 3); dims = 3)
-β_errors_avg     = dropdims(mean(β_errors;     dims = 3); dims = 3)
 
 formatter(x) = @sprintf("%.7f", x)
 to_pretty_table(x) = NamedArray(x, (collect(string.(priors_sym)), collect(hypothesis_to_inequalities.(hypotheses, k))), ("Prior", "Inequalities"))
@@ -91,6 +158,39 @@ to_pretty_table2(α_fam_errors)
 to_pretty_table2(α_errors)
 to_pretty_table2(β_errors)
 
+[string.(collect(hypothesis_to_inequalities.(hypotheses, k))); "average"]
+
+α_fam_errors2 = hcat(α_fam_errors, mean(view(α_fam_errors, :, 1:k-1); dims = 2))
+α_errors2     = hcat(α_errors,     mean(view(α_errors,     :, 1:k-1); dims = 2))
+β_errors2     = hcat(β_errors,     mean(view(β_errors,     :, 2:k);   dims = 2))
+
+xlabels = (1:6, [string.(collect(hypothesis_to_inequalities.(hypotheses, k))); "average"])
+xvalues = Float64.(reshape(repeat(1:length(hypotheses)+1, inner = size(α_fam_errors2, 1)), size(α_fam_errors2)...))
+jitterwidth = 0.25
+jittervalues = range(-jitterwidth, jitterwidth, length = size(xvalues, 1))
+jittervalues = jittervalues .- mean(jittervalues)
+for i in axes(xvalues, 2)
+	xvalues[:, i] .+= jittervalues
+end
+
+labels = get_labels(priors_sym)
+labels[2:3] .= labels[3:-1:2]
+colors = reshape(repeat(get_colors(priors_sym), size(α_fam_errors2, 2)), size(α_fam_errors2)...)
+shapes = reshape(repeat(get_shapes(priors_sym), size(α_fam_errors2, 2)), size(α_fam_errors2)...)
+
+function do_plot(errors, ylab; kwargs...)
+	plot(xvalues', errors',
+	labels = labels, color = colors', shape = permutedims(shapes),
+	foreground_color_legend = nothing, background_color_legend = nothing,
+	ylab = ylab, xlab = "No. inequalities", xticks = xlabels; kwargs...)
+end
+plt_α_fam = do_plot(α_fam_errors2, "Familywise α error"; yticks = 0:5:25, ylim = (0, 25), legend = (.7, 1))
+plt_α_err = do_plot(α_errors2,     "α error"; yticks = 0:5:20, ylim = (0, 20), legend = false)
+plt_β_err = do_plot(β_errors2,     "β error", legend = false)
+
+plt_joined = plot(plt_α_fam, plt_α_err, plt_β_err, layout = (1, 3), size = (3, 1).* 400,
+	bottom_margin = 6mm, left_margin = 8mm)
+savefig(plt_joined, joinpath("figures", "prior_performance.pdf"))
 
 
 
