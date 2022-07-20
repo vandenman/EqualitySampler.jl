7a1f0513688d92f7e532850366714bf4b0d6bd3b
vandenman <donvdbergh@hotmail.com>
2022-07-20 13:48:45 +0200

    actually push to gh-pages

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,8 @@ makedocs(
 	modules  = [EqualitySampler],
     format   = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
 )
+
+deploydocs(;
+    repo = "github.com/vandenman/EqualitySampler.git",
+    versions = ["stable" => "v^", "v#.#", "dev" => "main"]
+)
